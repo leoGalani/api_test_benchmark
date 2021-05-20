95a784f6e3362aef10af03a8ea1f2f2e790eb8c6
Leonardo Galani <leo@leonardobg.com.br>
2021-05-20 23:12:19 +0200

    removed asserts, since they are not  needed for this example

--- spec/copy_node-fecth-example.spec.js
+++ spec/copy_node-fecth-example.spec.js
@@ -6,11 +6,9 @@ describe("ServerRest", () => {
   test("get all products", async () => {
     const rawResponse = await fetch(url + "produtos");
     const produtos = await rawResponse.json();
-    expect(produtos.quantidade).toBe(3);
   });
   test("get all carinhos", async () => {
     const rawResponse = await fetch(url + "carrinhos");
     const carinhos = await rawResponse.json();
-    expect(carinhos.quantidade).toBe(1);
   });
 });
